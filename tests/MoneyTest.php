56bcb56a958497f3697859b84fc8c3ef5d018822
Yusuke Kageyama <kageyama_yusuke@aratana.jp>
2018-11-21 12:36:10 +0900

    理想形

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -33,5 +33,7 @@ final class MoneyTest extends CommonTestCase
         $five = new Doller(5);
         $five->times(2);
         $this->assertEquals(10, $five->amount);
+        $five->times(3);
+        $this->assertEquals(15, $five->amount);
     }
 }
