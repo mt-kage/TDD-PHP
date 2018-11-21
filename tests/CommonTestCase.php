ea13043752f649c60078bcdca0611a6f6787e595
Yusuke Kageyama <kageyama_yusuke@aratana.jp>
2018-11-21 11:00:22 +0900

    動くように調整

--- tests/CommonTestCase.php
+++ tests/CommonTestCase.php
@@ -2,22 +2,41 @@
 
 namespace Tdd;
 
-abstract class CommonTestCase extends \PHPUnit_Framework_TestCase
+/**
+ * Class CommonTestCase
+ * @package Tdd
+ */
+abstract class CommonTestCase extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * @var
+     */
     protected $expected;
+
+    /**
+     * @var
+     */
     protected $actual;
 
+    /**
+     *
+     */
     protected function setUp()
     {
     }
 
+    /**
+     *
+     */
     protected function tearDown()
     {
     }
 
+    /**
+     * @param null $message
+     */
     protected function verify($message = null)
     {
         $this->assertEquals($this->expected, $this->actual, $message);
     }
-
 }
