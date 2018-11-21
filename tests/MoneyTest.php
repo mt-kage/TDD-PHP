c8e89da50e589a8b1378a27a0437fe4cbaed1b61
Yusuke Kageyama <kageyama_yusuke@aratana.jp>
2018-11-21 11:19:46 +0900

    テストコード実装

---
+++ tests/MoneyTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tdd;
+
+/**
+ * Class MoneyTest
+ * @package Tdd
+ */
+class MoneyTest extends CommonTestCase
+{
+    /**
+     *
+     */
+    protected function setUp():void
+    {
+        parent::setUp();
+    }
+
+    /**
+     *
+     */
+    protected function tearDown():void
+    {
+        parent::tearDown();
+    }
+
+    /**
+     *
+     */
+    public function testMultiplication():void
+    {
+        $five = new Doller(5);
+        $five->times(2);
+        $this->assertEquals(10, $five->amount);
+    }
+}
